cbe36cfe3a5d6b8c35dc67ba5051858fff2ad61d
Paul-Gomez <paulgomezzubi@gmail.com>
2024-11-17 02:36:10 +0100

    Hago un esquema conceptual con como imagino que será el programa en mi cabeza.

--- src/GomezPaulEjercicio2.java
+++ src/GomezPaulEjercicio2.java
@@ -1,5 +1,31 @@
-public class Main {
+//Importo la clase scanner para leer la entrada del usuario.
+import java.util.Scanner;
+
+public class GomezPaulEjercicio2 {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+/*
+Esquema conceptual:
+
+    Solicito el carácter al usuario (System.out.print y char "pattern" scanner.
+
+    Solicito el tamaño al usuario (int size)
+
+        - Bucle do while para solicitar el valor entre 1-15. Si está fuera de rango (if).
+
+    Imprimir el tablero:
+
+        - Bucle for para las filas (8)
+        - Bucle for para la altura de cada casilla (utilizando size)
+        - Bucle for para las columnas (8)
+        - Bucle for para la anchura de cada casilla (utilizando size)
+
+    Determinar casillas:
+
+        - If else
+        - Utilizo fórmula. Si lo cumple es casilla con pattern. Si no, espacio en blanco.
+
+    Cierro scanner y programa
+
+ */
